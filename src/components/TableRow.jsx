e8e8ebf85c7838e9368fa96136c01bbdc9002876
saraivais <isadorasaraiva95@gmail.com>
2022-06-07 15:55:55 -0300

    feat: creates TableRow component~ receives an object as prop, renders data cells for each object key, minus residents~

---
+++ src/components/TableRow.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function TableRow(planetObject) {
+  const { planetObject: {
+    climate,
+    created,
+    diameter,
+    edited,
+    films,
+    gravity,
+    name,
+    orbital_period: orbitalPeriod,
+    population,
+    rotation_period: rotationPeriod,
+    surface_water: surfaceWater,
+    terrain,
+    url,
+  } } = planetObject;
+
+  return (
+    <tr>
+      <td>{ name }</td>
+      <td>{ rotationPeriod }</td>
+      <td>{ orbitalPeriod }</td>
+      <td>{ diameter }</td>
+      <td>{ climate }</td>
+      <td>{ gravity }</td>
+      <td>{ terrain }</td>
+      <td>{ surfaceWater }</td>
+      <td>{ population }</td>
+      <td>{ films }</td>
+      <td>{ created }</td>
+      <td>{ edited }</td>
+      <td>{ url }</td>
+    </tr>
+  );
+}
+
+export default TableRow;
