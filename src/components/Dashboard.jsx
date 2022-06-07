96ffe9b3ae45adc197da9329af03d9b4144f3636
saraivais <isadorasaraiva95@gmail.com>
2022-06-07 16:28:37 -0300

    feat: adds locally used useState to control input ~Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
 function Dashboard() {
+  const [nameFilter, setNameFilter] = useState('');
+
+  const handleInputChange = ({ target }) => {
+    setNameFilter(target.value);
+  }
+
   return (
     <div>
       <label htmlFor="name">
         Planet Name:
-        <input data-testid="name-filter" id="name" type="text" />
+        <input
+          data-testid="name-filter"
+          id="name"
+          type="text"
+          value={ nameFilter }
+          onChange={ handleInputChange }
+        />
       </label>
     </div>
   );
