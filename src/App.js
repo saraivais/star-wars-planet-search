62da5676221cfd37228dcfc20615406aac3e9ba7
saraivais <isadorasaraiva95@gmail.com>
2022-06-07 15:56:48 -0300

    feat: adds PlanetProvider and Table to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+// import PlanetContext from './context/MyContext';
 import './App.css';
+import PlanetProvider from './context/PlanetProvider';
+import Table from './components/Table';
 
 function App() {
   return (
-    <span>Hello, Emperor!</span>
+    <PlanetProvider>
+      <span>Hello, Emperor!</span>
+      <Table />
+    </PlanetProvider>
   );
 }
 
