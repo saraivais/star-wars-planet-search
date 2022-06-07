4f1c65dd4c5288e9de0df7b650c172212a5909f3
saraivais <isadorasaraiva95@gmail.com>
2022-06-07 16:15:59 -0300

    fix: changing data to filteredPlanets~

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,9 +3,9 @@ import PlanetContext from '../context/PlanetContext';
 import TableRow from './TableRow';
 
 function Table() {
-  const { planetData: { data } } = useContext(PlanetContext);
+  const { filteredPlanets } = useContext(PlanetContext);
   // console.log(data);
-  const isDataEmpty = data.length === 0;
+  const isDataEmpty = filteredPlanets.length === 0;
   // console.log(isDataEmpty);
   // console.log(Object.keys(data[0]));
 
@@ -13,7 +13,7 @@ function Table() {
     <table>
       <thead>
         <tr>
-          { !isDataEmpty && Object.keys(data[0])
+          { !isDataEmpty && Object.keys(filteredPlanets[0])
             .filter((category) => category !== 'residents')
             .map((category) => (
               <th key={ category }>{ category }</th>
@@ -21,7 +21,7 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        { !isDataEmpty && data
+        { !isDataEmpty && filteredPlanets
           .map((category, index) => <TableRow key={ index } planetObject={ category } />)}
 
       </tbody>
