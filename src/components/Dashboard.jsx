9d2e5429ea3d5153b77b708c305f41c50fda89a1
saraivais <isadorasaraiva95@gmail.com>
2022-06-07 19:01:18 -0300

    feat: adds select & input fields & button  ~Dashboard and adds another state to be used locally to control both selects and number input~

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,10 +2,16 @@ import React, { useState, useContext, useEffect } from 'react';
 import PlanetContext from '../context/PlanetContext';
 
 function Dashboard() {
+  const INITIAL_NUMERIC_VALUES = {
+    column: 'population',
+    comparison: 'maior que',
+    value: 100000,
+  };
   const [nameFilter, setNameFilter] = useState('');
+  const [numericFilter, setNumericFilter] = useState(INITIAL_NUMERIC_VALUES);
   const { nameFilter: { setFilterByName } } = useContext(PlanetContext);
 
-  const handleInputChange = ({ target }) => {
+  const handleNameInputChange = ({ target }) => {
     setNameFilter(target.value);
   };
 
@@ -22,9 +28,25 @@ function Dashboard() {
           id="name"
           type="text"
           value={ nameFilter }
-          onChange={ handleInputChange }
+          onChange={ handleNameInputChange }
         />
       </label>
+      <select name="column" data-testid="column-filter">
+        { /* population, orbital_period, diameter, rotation_period, surface_water */}
+      </select>
+      <select name="comparison" data-testid="comparison-filter">
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input name="value" data-testid="value-filter" type="number" />
+      <button
+        type="button"
+        data-testid="button-filter"
+      >
+        Filtrar
+
+      </button>
     </div>
   );
 }
