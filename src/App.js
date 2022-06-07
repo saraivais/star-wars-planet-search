f743900e64a3522ac2213a12136c7f8788e5a305
saraivais <isadorasaraiva95@gmail.com>
2022-06-07 16:19:26 -0300

    feat: imports and renders Dashboard on App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import PlanetProvider from './context/PlanetProvider';
 import Table from './components/Table';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <PlanetProvider>
       <span>Hello, Emperor!</span>
+      <Dashboard />
       <Table />
     </PlanetProvider>
   );
