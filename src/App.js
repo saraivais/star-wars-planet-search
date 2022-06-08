5964d8ed180a911dc839f2456748eeeb889321d0
saraivais <isadorasaraiva95@gmail.com>
2022-06-07 21:14:24 -0300

    feat: imports and renders CurrentFilters ~App

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import PlanetProvider from './context/PlanetProvider';
 import Table from './components/Table';
 import Dashboard from './components/Dashboard';
+import CurrentFilters from './components/CurrentFilters';
 
 function App() {
   return (
     <PlanetProvider>
       <span>Hello, Emperor!</span>
       <Dashboard />
+      <CurrentFilters />
       <Table />
     </PlanetProvider>
   );
