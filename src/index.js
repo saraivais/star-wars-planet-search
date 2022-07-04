62ef21412ba79c481697b36c8ae47e94eab4a251
saraivais <isadorasaraiva95@gmail.com>
2022-07-03 21:34:54 -0300

    style (index.js): imports index.css~

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import PlanetProvider from './context/PlanetProvider';
+import './index.css';
 
 ReactDOM.render(
   <PlanetProvider>
