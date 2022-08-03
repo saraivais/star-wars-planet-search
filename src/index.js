38dc5e66b18ad2ad36ac63a608339410d87257d4
saraivais <isadorasaraiva95@gmail.com>
2022-08-03 17:56:24 -0300

    fix [index]: removing index css from tailwind~

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import PlanetProvider from './context/PlanetProvider';
-import './index.css';
 
 ReactDOM.render(
   <PlanetProvider>
