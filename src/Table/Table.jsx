9c39d9de75d370c69f350bc9b64eecfbb90fe25d
saraivais <isadorasaraiva95@gmail.com>
2022-08-04 18:54:34 -0300

    style [Table]: adds classes for CSS styling~

--- src/Table/Table.jsx
+++ src/Table/Table.jsx
@@ -7,13 +7,13 @@ function Table() {
   const { filteredPlanets } = useContext(PlanetContext);
   const isDataEmpty = filteredPlanets.length === 0;
   return (
-    <table>
+    <table className="planet-table">
       <thead>
         <tr>
           { !isDataEmpty && Object.keys(filteredPlanets[0])
             .filter((category) => category !== 'residents')
             .map((category) => (
-              <th className="font-mono" key={ category }>{ category }</th>
+              <th className="table-headers" key={ category }>{ category }</th>
             ))}
         </tr>
       </thead>
